4e77ae0fc6f62ff1e69e2ca7ad869e69cf7ca9e7
hmswrth <mudra.hemanth@gmail.com>
2020-04-09 23:19:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,110 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+//document.querySelector('#current-'+ activePlayer).textContent = dice;
+
+//let x = document.querySelector('#score-0').textContent;
+//console.log(x);
+//set initial values  to zero
+
+
+//document.querySelector('btn-roll').addEventListener('click', btn); //btn is a callback function here cus event listener calls the function for us.
+//can also use an anonymous function so nobody can access it 
+let scores, roundScore, activePlayer, gamePlaying;
+init();
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
+
+        //random number with scope limited to the anonymous function
+        let dice = Math.floor((Math.random()) * 6) + 1;
+
+        //display the result
+        let diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        //update thr round score if dice is not 1
+        if (dice !== 1) {
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //next palyer
+            nextPlayer();
+
+        }
+    }
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+
+        //add current score to global
+        scores[activePlayer] += roundScore;
+
+        //UI update
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        //check if player has won
+        if (scores[activePlayer] >= 10) {
+            document.getElementById('name-' + activePlayer).textContent = 'Winner!';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
+
+});
+
+//changing player turns
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    //reset current scores to zero
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);  //call back function init
+
+//things that happen when the game is started!
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+}
